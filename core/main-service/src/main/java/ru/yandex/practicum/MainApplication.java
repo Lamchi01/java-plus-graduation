be65e5606d9134fd66d607176ce6b541d9a19663
Lamchi01 <sonic24072003@yandex.ru>
2025-05-13 22:59:35 +0300

    Доработка Gateway

--- core/main-service/src/main/java/ru/yandex/practicum/MainApplication.java
+++ core/main-service/src/main/java/ru/yandex/practicum/MainApplication.java
@@ -3,12 +3,10 @@ package ru.yandex.practicum;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
 public class MainApplication {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(MainApplication.class, args);
