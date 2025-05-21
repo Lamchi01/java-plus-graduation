bea84466832a0573cf5083269a86f49eb3980fef
Lamchi01 <sonic24072003@yandex.ru>
2025-05-21 17:33:30 +0300

    Переименовал сервис запросов

--- core/event-service/src/main/java/ru/yandex/practicum/controller/clients/RequestsClient.java
+++ core/event-service/src/main/java/ru/yandex/practicum/controller/clients/RequestsClient.java
@@ -3,6 +3,6 @@ package ru.yandex.practicum.controller.clients;
 import org.springframework.cloud.openfeign.FeignClient;
 import ru.yandex.practicum.feign.RequestServiceClient;
 
-@FeignClient(name = "requests-service", path = "/users")
+@FeignClient(name = "request-service", path = "/users")
 public interface RequestsClient extends RequestServiceClient {
 }
